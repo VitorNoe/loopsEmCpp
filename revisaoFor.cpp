9b832baaf4aab23c96db755c4019f6a7230be811
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-04-26 00:33:10 +0000

    Loops e arrays em C++

---
+++ revisaoFor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int array[] = {1,2,3,4,5,6,7,8,9,0};
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << array[i] << endl;
+    }
+    
+    return 0;
+}
